a18bfe0195771f512516947ab72a86baa4c1cbb6
SriHarshita2005 <sriharshita2005@gmail.com>
2026-05-27 14:10:50 +0530

    updated code

--- prime_numbers.py
+++ prime_numbers.py
@@ -67,6 +67,78 @@ def sum_of_n(n):
 # n=int(input())
 # sum_of_n(n)
 
+#Find the average of prime numbers in a range.
+def avg_of_n(n):
+    s=0
+    c=0
+    for i in range(1,n+1):
+        fc=0
+        for j in range(1,i+1):
+            if i%j==0:
+                fc+=1
+        if fc==2:
+            s+=i
+            c+=1
+    print(s/c)
+# n=int(input())
+# avg_of_n(n)
+
+#Print prime numbers between two given numbers.
+def prime_between_aandb(a,b):
+    for i in range(a,b+1):
+        fc=0
+        for j in range(1,i+1):
+            if i%j==0:
+                fc+=1
+        if fc==2:
+            print(i,end=" ")
+# a=int(input())
+# b=int(input())
+# prime_between_aandb(a,b)
+
+
+#Find the smallest prime number greater than N.
+def sp(n):
+    a=n
+    while True:
+        fc=0
+        for i in range(1,a+1):
+            if a%i==0:
+                fc+=1
+        if fc==2:
+            print(a)
+            break
+        a+=1
+# n=int(input())
+# sp(n)
+
+
+#Find the largest prime number less than N.
+def lp(n):
+    for i in range(n,1,-1):
+        fc=0
+        for j in range(1,i+1):
+            if i%j==0:
+                fc+=1
+        if fc==2:
+            print(i)
+            break
+# n=int(input())
+# lp(n)
+
+
+#Check whether a number is composite or prime.
+def is_prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            return "Composite number"
+        else:
+            return "Prime number"
+n=int(input())
+print(is_prime(n))
+
+
+
 
 
 
